024985966eddc346cd43d9e2c92ed61bb63dfc9c
TiagoSilva1991 <tiago.cerejeira.silva@gmail.com>
2025-04-06 17:31:26 +0100

    chore: cleanup

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 To run the project: 
     
-    - install dependencies: npm install
+  - install dependencies: npm install
 
-    - run server: npx json-server --watch db.json --port 3000
-    
-    - run app: npm run dev
+  - run server: npx json-server --watch db.json --port 3000
+  
+  - run app: npm run dev
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Footer = () => (
-    <footer className="app-footer">
-        <div className="app-container">
-            <div className="footer-content-wrapper">
-                <a href="https://www.linkedin.com/in/tiagocerejeirasilva/" target="_blank">Tiago Silva</a>
-                <p>Natixis Frontend Assessment - April 2025</p>
-            </div>
-        </div>
-    </footer>
+  <footer className="app-footer">
+    <div className="app-container">
+      <div className="footer-content-wrapper">
+        <a href="https://www.linkedin.com/in/tiagocerejeirasilva/" target="_blank">Tiago Silva</a>
+        <p>Natixis Frontend Assessment - April 2025</p>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
